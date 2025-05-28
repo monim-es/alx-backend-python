032caa76a33d72d29679e6076aa480290d49428a
monim-es <75100525+monim-es@users.noreply.github.com>
2025-05-29 00:04:35 +0100

    Update models.py

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+from django.contrib.auth.models import AbstractUser
+
+# Custom User model
+class User(AbstractUser):
+    bio = models.TextField(blank=True, null=True)
+    profile_pic = models.ImageField(upload_to='profiles/', blank=True, null=True)
+
+# Conversation model
+class Conversation(models.Model):
+    participants = models.ManyToManyField(User, related_name='conversations')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Conversation {self.id}"
+
+# Message model
+class Message(models.Model):
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name='messages')
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"From {self.sender.username} at {self.timestamp}"
