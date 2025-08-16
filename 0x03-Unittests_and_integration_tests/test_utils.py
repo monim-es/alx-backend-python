8b1ae07a8fe0cdf4b99c27ef1454ff9eb10f77f9
monim-es <75100525+monim-es@users.noreply.github.com>
2025-08-16 23:25:34 +0100

    Update test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -23,7 +23,8 @@ class TestAccessNestedMap(unittest.TestCase):
         ({}, ("a",), "a"),
         ({"a": 1}, ("a", "b"), "b"),
     ])
-    def test_access_nested_map_exception(self, nested_map, path, expected_key):
+    def test_access_nested_map_exception(self, nested_map, path,
+                                          expected_key):
         """Test access_nested_map raises KeyError for invalid paths."""
         with self.assertRaises(KeyError) as context:
             access_nested_map(nested_map, path)
